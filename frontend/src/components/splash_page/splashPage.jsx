1f64413d178b68e9daa614887b6fc32e5ae6689f
dpwong <dylanpwong@gmail.com>
2020-06-29 10:35:58 -0400

    sign up made

--- frontend/src/components/splash_page/splashPage.jsx
+++ frontend/src/components/splash_page/splashPage.jsx
@@ -3,16 +3,29 @@ import './splash.css';
 
 class SplashPage extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state={
+            ed: 'kim'
+        }
+
+        this.onSignUpClick=this.onSignUpClick.bind(this);
     }
 
+// this.props.history.push(/join);
+// this.setState={ed: 'kim2} | Causes re-render
+onSignUpClick(){
+    this.props.history('/join');
+}
 
-    render(){
+  render(){
         return(
             <>
             <div>
                 hello
             </div>
+
+            <div onClick={this.onSignUpClick}>Sign in</div>
             </>
 
         )
