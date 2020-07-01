3a25e3fdd8ee06805306329a4515fe1a59eb2b52
Edward Kim <eykim5@buffalo.edu>
2020-07-01 15:11:08 -0400

    Reformatted Deals section

--- frontend/src/components/splash_page/deals.jsx
+++ frontend/src/components/splash_page/deals.jsx
@@ -11,7 +11,10 @@ class Deals extends React.Component {
             <>
             <div className='deals'>
                 <div>Featured Deals</div>
+                <div className='images'>
                 <img src = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/447530/836913de4fdeec8f401687c5930fee4a91bf5197.jpg" ></img>
+                <img src = "https://cache.desktopnexus.com/wallpapers/2383/2383276-1920x1080-GAME-a064.jpg?st=-PdZ1vSFIEnn_afM99l0CQ&e=1593631500" ></img>
+                </div>
             </div>
             </>
         )
