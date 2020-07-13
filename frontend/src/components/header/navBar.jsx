a32fa7c1a8657437a762cebca0818e5074fd6472
Edward Kim <eykim5@buffalo.edu>
2020-07-13 16:15:42 -0400

    Added some stuff on splashPage and navBar, plus some reformatting in general

--- frontend/src/components/header/navBar.jsx
+++ frontend/src/components/header/navBar.jsx
@@ -21,7 +21,9 @@ class NavBar extends React.Component {
                 <div className="navbarItems">Steam Labs</div> */}
                 <div className="searchbarContainer">
                 <input placeholder="search the store" type="text" />
-
+                <div className="searchIcon">
+                    <img src="https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_1280.png"/>
+                </div>
                 </div>
             </div>
             </div>
--- frontend/src/components/splash_page/deals.jsx
+++ frontend/src/components/splash_page/deals.jsx
@@ -9,8 +9,21 @@ class Deals extends React.Component {
     render(){
         return (
             <>
+            <div className='gutterCol'>
+                <a>
+                <img src="https://steamstore-a.akamaihd.net/public/images/gift/steamcards_cards_02.png"></img>
+                </a>
+                <div className='gutterHeader'>GIFT CARDS</div>
+                    <div className='gutterContent'>Now Avaliable on Steam</div>
+                <div className='gutterHeader'>RECOMMENDED</div>
+                    <div className='gutterContent'>By Friends</div>
+                    <div className='gutterContent'>By Curators</div>
+                    <div className='gutterContent'>Tags</div>
+                <div className='gutterHeader'>BROWSE CATAGORIES</div>
+                <div className='gutterHeader'>BROWSE BY GENRE</div>
+            </div>
             <div className='deals'>
-                <div>Featured Deals</div>
+                <div>FEATURED & RECOMMENDED</div>
                 <div className='images'>
                 <img src = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/447530/836913de4fdeec8f401687c5930fee4a91bf5197.jpg" ></img>
                 <img src = "https://cache.desktopnexus.com/wallpapers/2383/2383276-1920x1080-GAME-a064.jpg?st=-PdZ1vSFIEnn_afM99l0CQ&e=1593631500" ></img>
