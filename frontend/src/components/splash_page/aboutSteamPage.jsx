32ebfc8280d37aa3763b670c61d84df5d640c015
Edward Kim <eykim5@buffalo.edu>
2020-08-02 15:27:26 -0400

    Updated About Steam page

--- frontend/src/components/splash_page/aboutSteamPage.jsx
+++ frontend/src/components/splash_page/aboutSteamPage.jsx
@@ -11,7 +11,15 @@ class AboutSteamPage extends React.Component {
             <>
             <div className="aboutSteamContainer">
                 <div className="aboutSteamHeader">About Steam</div>
-                <div className="aboutSteamContent">Steam is...</div>
+                <div className="aboutSteamContent">This project was made using the MERN Stack, a Javascript stack that consists of the following:</div>
+                <br></br>
+                <div className="aboutSteamContent">MongoDB, a NoSQL non-relational database.</div>
+                <br></br>
+                <div className="aboutSteamContent">Express, a backend web application framework.</div>
+                <br></br>
+                <div className="aboutSteamContent">React, a Javascript library built for user interfaces.</div>
+                <br></br>
+                <div className="aboutSteamContent">Node.js, a cross-platform Javascript runtime environment.</div>
             </div>
             </>
         )
