cb3ade803df79acd690a77c187136e81bc1c38f8
dpwong <dylanpwong@gmail.com>
2020-08-09 15:59:43 -0400

    review formatted update

--- frontend/src/components/game_page/review.jsx
+++ frontend/src/components/game_page/review.jsx
@@ -1,4 +1,5 @@
 import React from'react';
+import DateGetter from '../functions/date';
 
 
 class Review extends React.Component{
@@ -9,6 +10,23 @@ class Review extends React.Component{
           thumbsUp:"https://steamstore-a.akamaihd.net/public/shared/images/userreviews/icon_thumbsUp_v6.png",
           thumbsDown:"https://steamstore-a.akamaihd.net/public/shared/images/userreviews/icon_thumbsUp_v6.png",
         }
+
+        this.thumbsDecider=this.thumbsDecider.bind(this);
+    }
+
+    thumbsDecider(rev){
+      if(rev.recommendation){
+        return this.state.thumbsUp;
+      }else{
+        return this.state.thumbsDown;
+      }
+    }
+
+    strShuffler(str){
+      let arr = str.split("");
+      let result= arr.sort((a,b)=> Math.random()<.5? -1:1);
+      // debugger;
+      return result.join("");
     }
 
     render(){
@@ -22,7 +40,27 @@ class Review extends React.Component{
       }
 
       let reviews = this.props.rev.map(ele=>{
+        // debugger;
+        return(
+          
+          <div key={ this.strShuffler(ele._id)}>
+            <div className="reviewTop">
+              <div className="thumbsContainer">
+                  <img src={this.thumbsDecider(ele)}/>
+              </div>
+                  <div>{ele.recommendation? "Recommended": "Not Recommended"}</div>
+            </div>
+            <div className='dateWrapper'>
+              POSTED ON: &NBSP;
+              <DateGetter date={ele.date}/>
+            </div>
 
+            <div>
+              {ele.content}
+            </div>
+          </div>
+          
+        )
       }) 
         return (
           <>
@@ -30,15 +68,7 @@ class Review extends React.Component{
               <div className="aboutHeader">Customer Reviews</div>
 
               <div className="reviewContentWrapper">
-                  <div>
-
-                  </div>
-                  <div>
-                    <div> 
-                      
-                    </div>
-                    {this.props.rev[0].content}
-                  </div>
+                {reviews}
               </div>
             </div>
           </>
