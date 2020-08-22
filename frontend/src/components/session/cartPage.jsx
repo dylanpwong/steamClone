92ea2d93bc73d8811dac661a036aa6bc473c4b66
Edward Kim <eykim5@buffalo.edu>
2020-08-22 19:11:27 -0400

    Minor update to cart page

--- frontend/src/components/session/cartPage.jsx
+++ frontend/src/components/session/cartPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './miscPage.css'
 
 class cartPage extends React.Component {
 
     render() {
         return (
             <>
-            <div>Cart</div>
+            <div className="cartContainer">
+                <div className="cartHeader">Your Shopping Cart</div>
+                <div className="cartList">temp</div>
+            </div>
 
             </>
         )
