3eb01d321cee5461b80efcb66bed861c3d7e9572
Edward Kim <eykim5@buffalo.edu>
2020-08-22 01:01:16 -0400

    Minor update to formatting of wishList page

--- frontend/src/components/session/wishListPage.jsx
+++ frontend/src/components/session/wishListPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import './miscPage.css'
 
 class wishListPage extends React.Component {
 
     render() {
         return (
             <>
-            <div>Wishlist</div>
-
+            <div className="wishContainer">
+                <div className="wishHeading">Wishlist</div>
+                <div className="wishListContainer">
+                    <div className="wishGame">temp</div>
+                </div>
+            </div>
             </>
         )
     } 
