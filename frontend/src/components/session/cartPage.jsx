2ccb165975d6092016d755ed0cea8951699a95f3
Edward Kim <eykim5@buffalo.edu>
2020-08-29 16:04:08 -0400

    Updated cart page

--- frontend/src/components/session/cartPage.jsx
+++ frontend/src/components/session/cartPage.jsx
@@ -21,15 +21,36 @@ class cartPage extends React.Component {
 
     render() {
         if (!this.state.cart) return <> </>
-        // debugger;
+        const cartGames = this.state.cart.map((ele) => {
+            return (
+                <>
+                <div className="cartGame">
+                    <img className="cartImage" src={ele.imgUrl}></img>
+                    <div className="cartTitle">{ele.title}</div>
+                    <div className="cartPrice">{ele.price}</div>
+                </div>
+                </>
+            )
+        })
 
         return (
             <>
             <div className="cartContainer">
                 <div className="cartHeader">Your Shopping Cart</div>
-                <div className="cartList">temp</div>
+                <div className="cartList">{cartGames}</div>
+                <div className="cartFooter">
+                    <div className ="totalContainer">
+                        <div>Estimated total</div>
+                    </div>
+                    <div>Is this a purchase for yourself or is it a gift? Select one to continue to checkout.</div>
+                    <div className="checkoutContainer">
+                        <div className="checkoutButton">Purchase for myself</div>
+                        <div className="checkoutButton">Purchase as a gift</div>
+                    </div>
+                </div>
+                
+                <div className="continueButton">Continue Shopping</div>
             </div>
-
             </>
         )
         
