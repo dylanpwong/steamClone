4d7b5e3d55e8cad9651820633600c216a812b7b4
dpwong <dylanpwong@gmail.com>
2020-08-29 20:19:59 -0400

    check

--- frontend/src/components/session/cartPage.jsx
+++ frontend/src/components/session/cartPage.jsx
@@ -2,58 +2,65 @@ import React from 'react'
 import './miscPage.css'
 
 class cartPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            cart: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: null,
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.currentUser != null) {
+      this.props.getCart(this.props.currentUser.id).then((res) => {
+        this.setState({ cart: res.user.cart });
+      });
     }
+  }
+  stringToFloat(num) {
+    return parseFloat(num.substring(1));
+  }
 
-    componentDidMount(){
-        if (this.props.currentUser != null) {
+  render() {
+    if (!this.state.cart) return <> </>;
 
-            this.props.getCart(this.props.currentUser.id).then(res=>{
-                this.setState({cart: res.user.cart});
-            })
-        }
-    }
+    let totalPrice=0;
+    const cartGames = this.state.cart.map((ele) => {
+        totalPrice+= this.stringToFloat(ele.price);
+      return (
+        <>
+          <div className="cartGame">
+            <img className="cartImage" src={ele.imgUrl}></img>
+            <div className="cartTitle">{ele.title}</div>
+            <div className="cartPrice">{ele.price}</div>
+          </div>
+        </>
+      );
+    });
 
-    render() {
-        if (!this.state.cart) return <> </>
-        const cartGames = this.state.cart.map((ele) => {
-            return (
-                <>
-                <div className="cartGame">
-                    <img className="cartImage" src={ele.imgUrl}></img>
-                    <div className="cartTitle">{ele.title}</div>
-                    <div className="cartPrice">{ele.price}</div>
-                </div>
-                </>
-            )
-        })
-
-        return (
-            <>
-            <div className="cartContainer">
-                <div className="cartHeader">Your Shopping Cart</div>
-                <div className="cartList">{cartGames}</div>
-                <div className="cartFooter">
-                    <div className ="totalContainer">
-                        <div>Estimated total</div>
-                    </div>
-                    <div>Is this a purchase for yourself or is it a gift? Select one to continue to checkout.</div>
-                    <div className="checkoutContainer">
-                        <div className="checkoutButton">Purchase for myself</div>
-                        <div className="checkoutButton">Purchase as a gift</div>
-                    </div>
-                </div>
-                
-                <div className="continueButton">Continue Shopping</div>
+    return (
+      <>
+        <div className="cartContainer">
+          <div className="cartHeader">Your Shopping Cart</div>
+          <div className="cartList">{cartGames}</div>
+          <div className="cartFooter">
+            <div className="totalContainer">
+              <div>Estimated total</div>
             </div>
-            </>
-        )
-        
-    } 
+            <div>
+              Is this a purchase for yourself or is it a gift? Select one to
+              continue to checkout.
+            </div>
+            <div className="checkoutContainer">
+              <div className="checkoutButton">Purchase for myself</div>
+              <div className="checkoutButton">Purchase as a gift</div>
+            </div>
+          </div>
+
+          <div className="continueButton">Continue Shopping</div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default cartPage;
