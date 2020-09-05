03648027376c9c3e94d80337c86b14a5bba5dcfa
dpwong <dylanpwong@gmail.com>
2020-09-05 17:59:25 -0400

    added updated balance on add games

--- frontend/src/actions/game_actions.js
+++ frontend/src/actions/game_actions.js
@@ -95,7 +95,7 @@ export const receiveType = type =>{
 }
 // const error
 export const buyGames=(data)=>dispatch=>{
-    return APIutil.buyGames(data).then(res=> updateUser(res))
+    return APIutil.buyGames(data).then(res=> dispatch(updateUser(res)))
 }
 
 export const fetchHelltaker = () => dispatch => {
